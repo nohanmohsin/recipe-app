ce4f8088cb5d7f2fd7bc7607baee269f8001f2f4
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2021-06-28 21:27:25 +0600

    finished project

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -8,7 +8,7 @@ const Recipe = ({title, calories, image}) => {
                 <h1 className='foodname'>{title}</h1>
                 <span>{calories} cal</span>
                 <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis dui velit amet, cursus congue ultrices tellus enim a. Et, volutpat natoque tristique varius nunc dignissim. Arcu, mollis et interdum cras cursus. In enim, eget etiam interdum facilisis eget massa nullam ullamcorp</p>
-                <button>get Recipe</button>
+                <button>Get Recipe</button>
             </div>
             
         </div>
